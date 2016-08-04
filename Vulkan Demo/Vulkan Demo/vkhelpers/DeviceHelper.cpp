4f029f427aa114a36e5b702b00c4886e9ac58264
Kevin <dragn194@gmail.com>
2016-08-03 22:14:01 -0400

    Fix a typo in selectDeviceSurfaceFormat

--- Vulkan Demo/Vulkan Demo/vkhelpers/DeviceHelper.cpp
+++ Vulkan Demo/Vulkan Demo/vkhelpers/DeviceHelper.cpp
@@ -69,7 +69,7 @@ void DeviceHelper::createLogicalDevice() {
 
 VkSurfaceFormatKHR DeviceHelper::selectDeviceSurfaceFormat() {
 	VkSurfaceFormatKHR desiredFormat{ VK_FORMAT_B8G8R8A8_UNORM, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR };
-	if (physDeviceSwapDetails.formats.size == 1 && physDeviceSwapDetails.formats[0].format == VK_FORMAT_UNDEFINED) {
+	if (physDeviceSwapDetails.formats.size() == 1 && physDeviceSwapDetails.formats[0].format == VK_FORMAT_UNDEFINED) {
 		return desiredFormat;
 	}
 	for (auto format : physDeviceSwapDetails.formats) {
