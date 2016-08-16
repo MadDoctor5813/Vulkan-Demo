029193b1fadb50a39d6d659ccabe13efb6814e9e
Kevin <dragn194@gmail.com>
2016-08-16 12:46:00 -0400

    Add code to create VkShaderModules from files

--- Vulkan Demo/Vulkan Demo/loaders/ShaderLoader.cpp
+++ Vulkan Demo/Vulkan Demo/loaders/ShaderLoader.cpp
@@ -2,8 +2,12 @@
 
 #include <filesystem>
 #include <iostream>
+#include <fstream>
+#include <stdexcept>
 
-ShaderLoader::ShaderLoader() {
+#include "App.h"
+
+ShaderLoader::ShaderLoader(App& app) : appRef(app) {
 }
 
 
@@ -12,7 +16,10 @@ ShaderLoader::~ShaderLoader() {
 
 void ShaderLoader::loadShaders() {
 	for (auto iter = fs::directory_iterator("shaders/"); iter != fs::directory_iterator(); iter++) {
-		readBinaryFile(iter->path());
+		auto binaryData = readBinaryFile(iter->path());
+		VkWrapper<VkShaderModule> shaderModule{ appRef.getDeviceHelper().getDeviceWrapper(), vkDestroyShaderModule };
+		shaders.emplace(iter->path().string(), shaderModule);
+		createShaderModule(binaryData, shaders.at(iter->path().string()));
 	}
 }
 
@@ -21,11 +28,22 @@ VkPipelineShaderStageCreateInfo ShaderLoader::getShaderInfo(const std::string & 
 }
 
 std::vector<char> ShaderLoader::readBinaryFile(fs::path path) {
-	return std::vector<char>();
+	std::ifstream fStream(path.string(), std::ios::ate | std::ios::binary);
+	std::vector<char> data(fStream.tellg());
+	fStream.seekg(0);
+	fStream.read(data.data(), data.size());
+	fStream.close();
+	return data;
 }
 
-VkShaderModule ShaderLoader::createShaderModule(std::vector<char> data) {
-	return VkShaderModule();
+void ShaderLoader::createShaderModule(std::vector<char> data, VkWrapper<VkShaderModule>& module) {
+	VkShaderModuleCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+	createInfo.codeSize = data.size();
+	createInfo.pCode = (uint32_t*) data.data();
+	if (vkCreateShaderModule(appRef.getDeviceHelper().getDevice(), &createInfo, nullptr, &module) != VK_SUCCESS) {
+		throw std::runtime_error("Could not create shader module.");
+	}
 }
 
 VkPipelineShaderStageCreateInfo ShaderLoader::createShaderStage(VkShaderModule module) {
